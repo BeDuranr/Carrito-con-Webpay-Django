20a34b96df80e02dd6761d9f532760ab8e433f1e
BeDuranr <beduran.rubio@gmail.com>
2024-05-16 14:14:33 -0400

    apiClima

--- miproject/cars/api.js
+++ miproject/cars/api.js
@@ -2,8 +2,6 @@ if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(function(position) {
         var lat = position.coords.latitude;
         var lon = position.coords.longitude;
-        
-        // Llamada AJAX para enviar las coordenadas al servidor Django
         $.ajax({
             url: '/get-weather/',
             method: 'GET',
